9769f373b1cbd646fc1645eca1f724acdfd449d2
stanleyy30 <stanleyyoung300705@gmail.com>
2025-01-03 23:32:10 +0700

    Update FoodGramApplication.kt

--- app/src/main/java/com/example/foodgram/FoodGramApplication.kt
+++ app/src/main/java/com/example/foodgram/FoodGramApplication.kt
@@ -1,19 +1,1 @@
 package com.example.foodgram
-
-import android.app.Application
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-    name = "user_data"
-)
-
-open class FoodGramApplication : Application() {
-    lateinit var container: AppContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = DefaultAppContainer(dataStore)
-    }
-}
