58306e80e405829d2f2ff56b9c36fcadc253df84
Dannprand <153193400+Dannprand@users.noreply.github.com>
2025-01-03 21:48:54 +0700

    postView

--- app/src/main/java/com/example/foodgram/views/CreatePostView.kt
+++ app/src/main/java/com/example/foodgram/views/CreatePostView.kt
@@ -1,2 +1,15 @@
 package com.example.foodgram.views
 
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.foodgram.viewmodels.PostViewModel
+
+@Composable
+fun CreatePostView(
+    postViewModel: PostViewModel,
+    token: String,
+    modifier: Modifier = Modifier
+) {
+
+}
