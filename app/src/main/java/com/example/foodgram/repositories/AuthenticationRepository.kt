affee116581c96a8f950bd1d9a0bf40daf72e71b
stanleyy30 <stanleyyoung300705@gmail.com>
2025-01-03 22:38:42 +0700

    Update AuthenticationRepository.kt
    push AuthenticationRepo

--- app/src/main/java/com/example/foodgram/repositories/AuthenticationRepository.kt
+++ app/src/main/java/com/example/foodgram/repositories/AuthenticationRepository.kt
@@ -1,1 +1,35 @@
 package com.example.foodgram.repositories
+
+import com.example.foodgram.services.AuthenticationAPIService
+import com.example.foodgram.models.UserResponse
+import retrofit2.Call
+
+interface AuthenticationRepository {
+    fun register(username: String, email: String, password: String): Call<UserResponse>
+
+    fun login(username: String, password: String): Call<UserResponse>
+}
+
+class NetworkAuthenticationRepository(
+    private val authenticationAPIService: AuthenticationAPIService
+): AuthenticationRepository {
+    // Repository Register
+    override fun register(username: String, email: String, password: String): Call<UserResponse> {
+        var registerMap = HashMap<String, String>()
+
+        registerMap["username"] = username
+        registerMap["email"] = email
+        registerMap["password"] = password
+
+        return authenticationAPIService.register(registerMap)
+    }
+
+    override fun login(username: String, password: String): Call<UserResponse> {
+        var loginMap = HashMap<String, String>()
+
+        loginMap["username"] = username
+        loginMap["password"] = password
+
+        return authenticationAPIService.login(loginMap)
+    }
+}
