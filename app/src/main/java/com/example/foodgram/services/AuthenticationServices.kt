b32b960aef0864cc233d201b8bb95b5bf925f76a
stanleyy30 <stanleyyoung300705@gmail.com>
2025-01-03 22:34:16 +0700

    Update AuthenticationServices.kt
    push AuthenticationService

--- app/src/main/java/com/example/foodgram/services/AuthenticationServices.kt
+++ app/src/main/java/com/example/foodgram/services/AuthenticationServices.kt
@@ -1,1 +1,14 @@
 package com.example.foodgram.services
+
+import com.example.foodgram.models.UserResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthenticationAPIService {
+    @POST("api/register")
+    fun register(@Body registerMap: HashMap<String, String> ): Call<UserResponse>
+
+    @POST("api/login")
+    fun login(@Body loginMap: HashMap<String, String>): Call<UserResponse>
+}
