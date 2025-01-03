141477d7eea7e3ee81d233cec003396dc2179bef
Dannprand <153193400+Dannprand@users.noreply.github.com>
2025-01-03 23:44:08 +0700

    MainActivity

--- app/src/main/java/com/example/foodgram/FoodGramApplication.kt
+++ app/src/main/java/com/example/foodgram/FoodGramApplication.kt
@@ -1,4 +1,19 @@
 package com.example.foodgram
 
-class FoodGramApplication {
+import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "user_data"
+)
+
+open class FoodGramApplication : Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer(dataStore)
+    }
 }
