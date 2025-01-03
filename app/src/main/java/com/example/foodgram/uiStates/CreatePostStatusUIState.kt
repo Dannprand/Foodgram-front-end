c92606a3deffd1f0dda892d763a32af9b50fe104
Dannprand <153193400+Dannprand@users.noreply.github.com>
2025-01-03 23:18:53 +0700

    UIStatePost

--- app/src/main/java/com/example/foodgram/uiStates/CreatePostStatusUIState.kt
+++ app/src/main/java/com/example/foodgram/uiStates/CreatePostStatusUIState.kt
@@ -1,4 +1,10 @@
 package com.example.foodgram.uiStates
 
-interface CreatePostStatusUIState {
+import com.example.foodgram.models.CreatePostModel
+
+sealed interface CreatePostStatusUIState {
+    data class Success(val data: CreatePostModel): CreatePostStatusUIState
+    object Start: CreatePostStatusUIState
+    object Loading: CreatePostStatusUIState
+    data class Failed(val errorMessage:String): CreatePostStatusUIState
 }
