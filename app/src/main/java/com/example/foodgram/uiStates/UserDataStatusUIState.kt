920023aff3a388bf422c8bc5c4fa77169151864f
stanleyy30 <stanleyyoung300705@gmail.com>
2025-01-03 23:17:17 +0700

    push UserUIStates

--- app/src/main/java/com/example/foodgram/uiStates/UserDataStatusUIState.kt
+++ app/src/main/java/com/example/foodgram/uiStates/UserDataStatusUIState.kt
@@ -1,4 +1,10 @@
 package com.example.foodgram.uiStates
 
-interface UserDataStatusUIState {
+import com.example.foodgram.models.GetUserModel
+
+sealed interface UserDataStatusUIState {
+    data class Success(val data: GetUserModel): UserDataStatusUIState
+    object Start: UserDataStatusUIState
+    object Loading: UserDataStatusUIState
+    data class Failed(val errorMessage: String): UserDataStatusUIState
 }
