50ca6cfa4976232cd296b41638c33f4e621ed432
stanleyy30 <stanleyyoung300705@gmail.com>
2025-01-03 22:41:55 +0700

    Update AuthenticationViewModel.kt
    push AutenticationVM

--- app/src/main/java/com/example/foodgram/viewmodels/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/foodgram/viewmodels/AuthenticationViewModel.kt
@@ -1,4 +1,280 @@
 package com.example.foodgram.viewmodels
 
-class AuthenticationViewModel {
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.NavHostController
+import com.example.foodgram.FoodGramApplication
+import com.example.foodgram.R
+import com.example.foodgram.models.UserResponse
+import com.example.foodgram.repositories.AuthenticationRepository
+import com.example.foodgram.repositories.UserRepository
+import com.example.foodgram.enums.PagesEnum
+import com.example.foodgram.uiStates.AuthenticationStatusUIState
+import com.example.foodgram.uiStates.AuthenticationUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+
+class AuthenticationViewModel(
+    private val authenticationRepository: AuthenticationRepository,
+    private val userRepository: UserRepository
+) : ViewModel() {
+    private val _authenticationUIState = MutableStateFlow(AuthenticationUIState())
+
+    val authenticationUIState: StateFlow<AuthenticationUIState>
+        get() {
+            return _authenticationUIState.asStateFlow()
+        }
+
+    var dataStatus: AuthenticationStatusUIState by mutableStateOf(AuthenticationStatusUIState.Start)
+        private set
+
+    var usernameInput by mutableStateOf("")
+        private set
+
+    var passwordInput by mutableStateOf("")
+
+    var emailInput by mutableStateOf("")
+        private set
+
+    fun changeEmailInput(emailInput: String) {
+        this.emailInput = emailInput
+    }
+
+    fun changeUsernameInput(usernameInput: String) {
+        this.usernameInput = usernameInput
+    }
+
+    fun changePasswordInput(passwordInput: String) {
+        this.passwordInput = passwordInput
+    }
+
+    fun checkLoginForm() {
+        if (usernameInput.isNotEmpty() && passwordInput.isNotEmpty()) {
+            _authenticationUIState.update { currentState ->
+                currentState.copy(
+                    buttonEnabled = true
+                )
+            }
+        } else {
+            _authenticationUIState.update { currentState ->
+                currentState.copy(
+                    buttonEnabled = false
+                )
+            }
+        }
+    }
+
+    fun checkRegisterForm() {
+        if (emailInput.isNotEmpty() && passwordInput.isNotEmpty() && usernameInput.isNotEmpty()) {
+            _authenticationUIState.update { currentState ->
+                currentState.copy(
+                    buttonEnabled = true
+                )
+            }
+        } else {
+            _authenticationUIState.update { currentState ->
+                currentState.copy(
+                    buttonEnabled = false
+                )
+            }
+        }
+    }
+
+    fun changePasswordVisibility() {
+        _authenticationUIState.update { currentState ->
+            if (currentState.showPassword) {
+                currentState.copy(
+                    showPassword = false,
+                    passwordVisibility = PasswordVisualTransformation(),
+                    passwordVisibilityIcon = R.drawable.ic_password_visible
+                )
+            } else {
+                currentState.copy(
+                    showPassword = true,
+                    passwordVisibility = VisualTransformation.None,
+                    passwordVisibilityIcon = R.drawable.ic_password_invisible
+                )
+            }
+        }
+    }
+
+    fun checkButtonEnabled(isEnabled: Boolean): Color {
+        if (isEnabled) {
+            return Color.White
+        }
+
+        return Color.DarkGray.copy(alpha = 0.75f)
+    }
+
+    fun checkRegisterFormValidation(): String {
+        if (passwordInput.count() < 8) {
+            return "Password must contain atleast 8 characters"
+        }
+
+        if (usernameInput.trim().count() < 3) {
+            return "Username must contain atleast 3 characters"
+        }
+
+        if (emailInput.trim().isEmpty()) {
+            return "Email must not be empty"
+        }
+
+        val emailRegex = Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+
+        if (!emailRegex.matches(emailInput)) {
+            return "Invalid email format"
+        }
+
+        return ""
+    }
+
+    fun checkLoginFormValidation(): String {
+        if (passwordInput.count() < 8) {
+            return "Password must contain atleast 8 characters"
+        }
+
+        if (usernameInput.trim().count() < 3) {
+            return "Username must contain atleast 3 characters"
+        }
+
+        return ""
+    }
+
+    fun registerUser(navController: NavHostController) {
+        viewModelScope.launch {
+            dataStatus = AuthenticationStatusUIState.Loading
+
+            try {
+                val call = authenticationRepository.register(usernameInput, emailInput, passwordInput)
+
+                call.enqueue(object: Callback<UserResponse> {
+                    override fun onResponse(call: Call<UserResponse>, res: Response<UserResponse>) {
+                        if (res.isSuccessful) {
+                            Log.d("response-data", "RESPONSE DATA: ${res.body()}")
+
+                            saveUserIDToken(res.body()!!.data.token!!, res.body()!!.data.user_id.toString()) {
+
+                                dataStatus = AuthenticationStatusUIState.Success(res.body()!!.data)
+
+                                resetViewModel()
+
+                                navController.navigate(PagesEnum.Profile.name) {
+                                    popUpTo(PagesEnum.Register.name) {
+                                        inclusive = true
+                                    }
+                                }
+                            }
+                        } else {
+                            dataStatus = AuthenticationStatusUIState.Failed(res.message())
+                        }
+                    }
+
+                    override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                        Log.d("error-data", "ERROR DATA: ${t.localizedMessage}")
+                        dataStatus = AuthenticationStatusUIState.Failed(t.localizedMessage)
+                    }
+
+                })
+            } catch (error: IOException) {
+                dataStatus = AuthenticationStatusUIState.Failed(error.localizedMessage)
+                Log.d("register-error", "REGISTER ERROR: ${error.localizedMessage}")
+            }
+        }
+    }
+
+    fun loginUser(
+        navController: NavHostController
+    ) {
+        viewModelScope.launch {
+            dataStatus = AuthenticationStatusUIState.Loading
+            try {
+                val call = authenticationRepository.login(usernameInput, passwordInput)
+                call.enqueue(object: Callback<UserResponse> {
+                    override fun onResponse(call: Call<UserResponse>, res: Response<UserResponse>) {
+                        if (res.isSuccessful) {
+                            saveUserIDToken(res.body()!!.data.token!!, res.body()!!.data.user_id.toString()) {
+
+                                dataStatus = AuthenticationStatusUIState.Success(res.body()!!.data)
+
+                                resetViewModel()
+
+                                navController.navigate(PagesEnum.Profile.name) {
+                                    popUpTo(PagesEnum.Login.name) {
+                                        inclusive = true
+                                    }
+                                }
+                            }
+                        } else {
+                            dataStatus = AuthenticationStatusUIState.Failed(res.message())
+                        }
+                    }
+
+                    override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                        dataStatus = AuthenticationStatusUIState.Failed(t.localizedMessage)
+                    }
+
+                })
+            } catch (error: IOException) {
+                dataStatus = AuthenticationStatusUIState.Failed(error.localizedMessage)
+                Log.d("register-error", "LOGIN ERROR: ${error.toString()}")
+            }
+        }
+    }
+
+    fun saveUserIDToken(token: String, userID: String, afterAction:() -> Unit) {
+        viewModelScope.launch {
+            userRepository.saveUserID(userID)
+            userRepository.saveUserToken(token)
+
+            afterAction.invoke()
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as FoodGramApplication)
+                val authenticationRepository = application.container.authenticationRepository
+                val userRepository = application.container.userRepository
+                AuthenticationViewModel(authenticationRepository, userRepository)
+            }
+        }
+    }
+
+    fun resetViewModel() {
+        changeEmailInput("")
+        changePasswordInput("")
+        changeUsernameInput("")
+        _authenticationUIState.update { currentState ->
+            currentState.copy(
+                showPassword = false,
+                passwordVisibility = PasswordVisualTransformation(),
+                passwordVisibilityIcon = R.drawable.ic_password_visible,
+                buttonEnabled = false
+            )
+        }
+        dataStatus = AuthenticationStatusUIState.Start
+    }
+
+    fun clearErrorMessage() {
+        dataStatus = AuthenticationStatusUIState.Start
+    }
 }
